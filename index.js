b12825411af25ac7c36408858ccbc7326b000fd2
Parker Crosgrove <pdxparker3@gmail.com>
2023-01-21 19:38:24 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Remove the <main> element
+var main = document.getElementById('main');
+main.parentNode.removeChild(main);
+
+// Create a new <h1> element
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Parker is the champion';
+
+// Insert the new <h1> element into the <body>
+document.body.appendChild(newHeader);
